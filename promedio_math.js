762d6cd23c3dc98edee80f56ff0a621b1e9118bb
yeison <bedoyadt@gmail.com>
2022-10-03 11:35:10 -0500

    octener el numero que este mas veses en un odjecto o une arrary

--- promedio_math.js
+++ promedio_math.js
@@ -64,3 +64,30 @@ function ordenarLista(listaDesordenada) {
   const lista = listaDesordenada.sort(ordenarListaSort);
   return lista;
 }
+
+function calcularModa(lista) {
+  const listaCount = {};
+
+  for (let i = 0; i < lista.length; i++) {
+    const elemento = lista[i];
+
+    if (listaCount[elemento]) {
+      listaCount[elemento] += 1;
+    } else {
+      listaCount[elemento] = 1;
+    }
+  }
+  const listaArray = Object.entries(listaCount);
+  const listaOrdenada = ordenarListaBidimensional(listaArray, 1);
+  const listaDesordenadaMaxNumber = listaOrdenada[listaOrdenada.length - 1];
+  //console.log("la moda es" + listaDesordenadaMaxNumber[0]);
+  const moda = listaDesordenadaMaxNumber[0];
+  return moda;
+}
+function ordenarListaBidimensional(listaDesordenada, i) {
+  function ordenarListaSort(valorAcumulado, nuevoValor) {
+    return valorAcumulado[i] - nuevoValor[i];
+  }
+  const lista = listaDesordenada.sort(ordenarListaSort);
+  return lista;
+}
