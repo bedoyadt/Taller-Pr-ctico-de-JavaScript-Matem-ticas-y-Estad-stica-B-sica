c50d34ebfb771bfbc66e071ff087fce5ba10e89c
yeison <bedoyadt@gmail.com>
2022-10-03 10:28:55 -0500

    ordenar lista

--- promedio_math.js
+++ promedio_math.js
@@ -50,3 +50,17 @@ function calcularMediana(lista) {
     return medianaListaImpar;
   }
 }
+
+function ordenarLista(listaDesordenada) {
+  function ordenarListaSort(valorAcumulado, nuevoValor) {
+    if (valorAcumulado > nuevoValor) {
+      return 1;
+    } else if (valorAcumulado == nuevoValor) {
+      return 0;
+    } else if (valorAcumulado < nuevoValor) {
+      return -1;
+    }
+  }
+  const lista = listaDesordenada.sort(ordenarListaSort);
+  return lista;
+}
